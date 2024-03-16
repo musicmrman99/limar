d60c727f4bcc00c82be536302be16462aec7a420
William Taylor <williamtaylordev@gmail.com>
2024-03-16 14:48:30 +0000

    Add `@uris` as standard for `manifest`

---
+++ modules/manifest_context_uris.py
@@ -0,0 +1,13 @@
+from modules.manifest_modules import uris_local, uris_remote
+
+# Types
+from core.modulemanager import ModuleManager
+
+class ManifestContextUris():
+
+    # Lifecycle
+    # --------------------
+
+    def configure(self, *, mod: ModuleManager, **_):
+        mod.manifest().add_context_module(uris_local.UrisLocal)
+        mod.manifest().add_context_module(uris_remote.UrisRemote)
