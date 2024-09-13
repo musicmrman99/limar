f4f85ce4c2b7f4bdf8513ee4b43db68f7db1d352
William Taylor <williamtaylordev@gmail.com>
2024-09-13 21:13:47 +0100

    Refactor `info` to use a service method

--- modules/info.py
+++ modules/info.py
@@ -2,6 +2,7 @@ import random
 import string
 import subprocess
 
+from core.modulemanager import ModuleAccessor
 from core.modules.phase_utils.phase_system import PhaseSystem
 
 # Types
@@ -12,9 +13,7 @@ INFO_LIFECYCLE = PhaseSystem(
     f'{__name__}:lifecycle',
     (
         'INITIALISE',
-        'QUERY',
         'GET',
-        'MERGE',
         'TABULATE',
         'RENDER'
     ),
@@ -44,6 +43,9 @@ class InfoModule:
     def configure(self, *, mod: Namespace, **_):
         mod.phase.register_system(INFO_LIFECYCLE)
 
+    def start(self, *, mod: Namespace, **_):
+        self._mod = mod
+
     def __call__(self, *,
             mod: Namespace,
             args: Namespace,
@@ -57,67 +59,8 @@ class InfoModule:
 
         output: Any = forwarded_data
 
-        query_command_items = {}
-        if transition_to_phase(INFO_LIFECYCLE.PHASES.QUERY):
-            ref = 'info-query-'+''.join(random.choices(string.hexdigits, k=32))
-            # FIXME: Yes, I know, this is an injection attack waiting to happen.
-            mod.manifest.declare_item_set(ref, f'query & [{args.entity}]')
-            query_command_items = mod.manifest.get_item_set(ref)
-            mod.log.debug(f'Matched manifest items:', query_command_items)
-
-        # Execute each query. Produces a list of entity data (inc. entity ID)
-        # for each query executed.
-        #   ItemSet -> list[list[dict[str, str]]]
         if transition_to_phase(INFO_LIFECYCLE.PHASES.GET):
-            output = []
-            for item in query_command_items.values():
-                command_query = (
-                    item['command']['parse']
-                    if 'parse' in item['command']
-                    else '.'
-                )
-
-                command_outputs: list[dict[str, Any]] = []
-                for command in item['command']['commands']:
-                    try:
-                        subproc = subprocess.Popen(
-                            command['command'],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE
-                        )
-                        stdout, stderr = subproc.communicate()
-                        command_status = 0
-                    except subprocess.CalledProcessError as e:
-                        if not command['allowedToFail']:
-                            raise e
-                        command_status = e.returncode
-
-                    command_outputs.append({
-                        'status': command_status,
-                        'stdout': stdout.decode().strip(),
-                        'stderr': stderr.decode().strip()
-                    })
-
-                output.append(mod.tr.query(
-                    command_query,
-                    command_outputs,
-                    lang='jq',
-                    first=True
-                ))
-
-            mod.log.debug(f'Query output:', output)
-
-        # Index and merge entity data by ID
-        #   list[list[dict[str, str]]] -> dict[str, dict[str, str]]
-        if transition_to_phase(INFO_LIFECYCLE.PHASES.MERGE):
-            tmp_output: dict[str, dict[str, str]] = {}
-            for entity_list in output:
-                for entity_data in entity_list:
-                    id = entity_data['id']
-                    if id not in tmp_output:
-                        tmp_output[id] = {}
-                    tmp_output[id].update(entity_data)
-            output = tmp_output
+            output = self.get(args.entity)
 
         # Format
         if transition_to_phase(
@@ -132,3 +75,73 @@ class InfoModule:
 
         # Forward
         return output
+
+    @ModuleAccessor.invokable_as_service
+    def get(self, entity_spec: str) -> dict[str, dict[str, str]]:
+        """
+        Get the list of the queries in the command manifest that match the given
+        command_spec, then return the indexed list of objects returned by those
+        queries.
+        """
+
+        query_command_items = {}
+
+        # Query the command manifest
+        ref = 'info-query-'+''.join(random.choices(string.hexdigits, k=32))
+        # FIXME: Yes, I know, this is an injection attack waiting to happen.
+        self._mod.manifest.declare_item_set(ref, f'query & [{entity_spec}]')
+        query_command_items = self._mod.manifest.get_item_set(ref)
+        self._mod.log.debug(f'Matched manifest items:', query_command_items)
+
+        # Execute each query. Produces a list of entity data (inc. entity ID)
+        # for each query executed.
+        #   ItemSet -> list[list[dict[str, str]]]
+        query_outputs = []
+        for item in query_command_items.values():
+            command_query = (
+                item['command']['parse']
+                if 'parse' in item['command']
+                else '.'
+            )
+
+            command_outputs: list[dict[str, Any]] = []
+            for command in item['command']['commands']:
+                try:
+                    subproc = subprocess.Popen(
+                        command['command'],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                    )
+                    stdout, stderr = subproc.communicate()
+                    command_status = 0
+                except subprocess.CalledProcessError as e:
+                    if not command['allowedToFail']:
+                        raise e
+                    command_status = e.returncode
+
+                command_outputs.append({
+                    'status': command_status,
+                    'stdout': stdout.decode().strip(),
+                    'stderr': stderr.decode().strip()
+                })
+
+            query_outputs.append(self._mod.tr.query(
+                command_query,
+                command_outputs,
+                lang='jq',
+                first=True
+            ))
+
+        self._mod.log.debug(f'Query output:', query_outputs)
+
+        # Index and merge entity data by ID
+        #   list[list[dict[str, str]]] -> dict[str, dict[str, str]]
+        merged_query_output: dict[str, dict[str, str]] = {}
+        for entity_list in query_outputs:
+            for entity_data in entity_list:
+                id = entity_data['id']
+                if id not in merged_query_output:
+                    merged_query_output[id] = {}
+                merged_query_output[id].update(entity_data)
+
+        return merged_query_output
